7ca34576c4993f9dbd67d173097efec056158e0d
knht <43456808+knht@users.noreply.github.com>
2024-11-02 02:51:56 +0100

    chore: Delete accidentally tracked files

--- node_modules/tsup/dist/cli-main.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});
-
-var _chunkSNM7IVOJjs = require('./chunk-SNM7IVOJ.js');
-require('./chunk-TWGB4FYO.js');
-require('./chunk-TWFEYLU4.js');
-
-
-exports.main = _chunkSNM7IVOJjs.main;
